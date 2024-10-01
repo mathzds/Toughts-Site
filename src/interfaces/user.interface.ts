8b10d7bbc69bfd93e361655ecdd025b2c395e963
mathzds <pedrofilhodev@gmail.com>
2024-10-01 16:43:38 -0300

    user page

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,4 +1,5 @@
 export default interface UserInterface {
+	username: string;
 	name: string;
 	email: string;
 	id: number;
--- src/pages/userAccount/userAccount.tsx
+++ src/pages/userAccount/userAccount.tsx
@@ -1,7 +1,51 @@
+import { jwtDecode } from "jwt-decode";
+import { useEffect, useState } from "react";
+import type UserInterface from "@/interfaces/user.interface";
+import axios from "axios";
+
 function UserAccount() {
+	const [user, setUser] = useState<UserInterface | null>(null);
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const decodeToken = async () => {
+			const token = localStorage.getItem("token");
+			if (token) {
+				try {
+					const decoded = jwtDecode<UserInterface>(token);
+
+					const response = await axios.get(
+						`http://localhost:3000/users/${decoded.id}`,
+					);
+					setUser(response.data);
+					console.log(response.data);
+				} catch (error) {
+					console.error("Token decoding failed:", error);
+				} finally {
+					setLoading(false);
+				}
+			} else {
+				setLoading(false);
+			}
+		};
+
+		decodeToken();
+	}, []);
+
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen text-center">
-			<h1 className="text-2xl font-bold mt-4 mb-4">User </h1>
+			<h1 className="text-2xl font-bold mt-4 mb-4">User Account</h1>
+			{loading ? (
+				<p>Loading...</p>
+			) : user ? (
+				<div>
+					<p>Name: {user.name || user.username || "N/A"}</p>
+					<p>Email: {user.email || "N/A"}</p>
+					<p>Age: {user.id || "N/A"}</p>
+				</div>
+			) : (
+				<p>No user data available.</p>
+			)}
 		</div>
 	);
 }
